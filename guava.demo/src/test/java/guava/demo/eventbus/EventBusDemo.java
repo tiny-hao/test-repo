bf498e0384d6f1fb49a4fa0987f964c35f849011
tiny-hao <181832018@qq.com>
2018-10-01 19:05:57 +0800

    guava test

---
+++ guava.demo/src/test/java/guava/demo/eventbus/EventBusDemo.java
@@ -0,0 +1,21 @@
+package guava.demo.eventbus;
+
+import org.junit.Test;
+
+import com.google.common.eventbus.EventBus;
+
+
+public class EventBusDemo {
+	@Test
+	public void test() {
+		EventBus eventBus = new EventBus("jack");
+		/*
+		 * 如果多个subscriber订阅了同一个事件,那么每个subscriber都将收到事件通知 并且收到事件通知的顺序跟注册的顺序保持一致
+		 */
+		eventBus.register(new EventListener()); // 注册订阅者
+		eventBus.register(new MultiEventListener());
+		eventBus.post(new OrderEvent("hello")); // 发布事件
+		eventBus.post(new OrderEvent("world"));
+		eventBus.post("!");
+	}
+}
